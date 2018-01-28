32bb0a72b9f92825eeca69a7312c078c43c433bf
sivas <sivas@enceladus.fritz.box>
2018-01-28 21:45:42 +0100

    1st running WS

---
+++ dawn-odd/dawn-odd-logic/src/main/java/de/elsivas/odd/logic/clock/bin/BinClockPrettyPrinter.java
@@ -0,0 +1,9 @@
+package de.elsivas.odd.logic.clock.bin;
+
+public class BinClockPrettyPrinter {
+	
+	public void print() {
+		
+	}
+
+}
---
+++ dawn-odd/dawn-odd-logic/src/test/java/de/elsivas/odd/BinClockTest.java
@@ -0,0 +1,37 @@
+package de.elsivas.odd;
+
+import org.junit.Test;
+
+import de.elsivas.odd.logic.clock.bin.BinaryClock;
+
+public class BinClockTest {
+
+	@Test
+	public void test2() {
+		BinaryClock.run();
+		while (true) {
+			sleep(20 * 1000);
+		}
+	}
+
+	@Test
+	public void test() {
+		BinaryClock.run();
+		sleep(10 * 1000);
+		BinaryClock.addAlarm("0100");
+		int i = 2;
+		while (i > 1) {
+			sleep(1000);
+		}
+		BinaryClock.stop();
+	}
+
+	private void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
