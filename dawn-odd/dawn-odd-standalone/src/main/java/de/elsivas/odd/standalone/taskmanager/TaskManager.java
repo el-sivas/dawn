ddde28e2deae83ee016b0b95336974937f5f91bf
sivas <sivas@enceladus.fritz.box>
2018-02-19 21:51:30 +0100

    Version 0.3

--- dawn-odd/dawn-odd-standalone/src/main/java/de/elsivas/odd/standalone/taskmanager/TaskManager.java
+++ dawn-odd/dawn-odd-standalone/src/main/java/de/elsivas/odd/standalone/taskmanager/TaskManager.java
@@ -26,7 +26,7 @@ public class TaskManager {
 
 	private static final Log LOG = SimpleLogFactory.getLog(TaskManager.class);
 
-	private static final String VERSION = "0.3-SN";
+	private static final String VERSION = "0.3";
 
 	private final TaskFileDao dao = new TaskFileDao();
 
