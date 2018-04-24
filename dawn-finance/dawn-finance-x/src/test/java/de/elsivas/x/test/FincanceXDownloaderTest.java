3612a04020c3e5baec5c70d4f4f71d7961a2be0a
sebastian <sebastian@kallisto.fritz.box>
2018-04-24 19:15:14 +0200

    fin dl onvista test

---
+++ dawn-finance/dawn-finance-x/src/test/java/de/elsivas/x/test/FincanceXDownloaderTest.java
@@ -0,0 +1,35 @@
+package de.elsivas.x.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.elsivas.finance.download.FinanceXDownloader;
+import de.elsivas.finance.logic.Wertpapier;
+import de.elsivas.finance.logic.config.FinConfig;
+import de.elsivas.finance.logic.portals.Portal;
+
+public class FincanceXDownloaderTest {
+	
+	private FinanceXDownloader financeXDownloader;
+	private Map<String, String> config = new HashMap<>();
+	
+	@Before
+	public void init() {
+		Configurator.setLevel("de.elsivas", Level.DEBUG);
+		financeXDownloader = new FinanceXDownloader();
+		config.put("portal", Portal.ONVISTA.toString());
+		config.put("download-list", Wertpapier.DAX.toString());
+		FinConfig.init(config);
+	}
+	
+	@Test
+	public void test() {
+		financeXDownloader.run();
+	}
+
+}
